4ab8b6138f228628a42d713be4bb361769a6f65d
lizandrof273 <lizandrof273@gmail.com>
2019-10-26 17:03:04 -0700

    Basic idea

--- Encourager/app/src/main/java/com/example/encourager/MainActivity.java
+++ Encourager/app/src/main/java/com/example/encourager/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.encourager;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button submitButton;
+    private TextView encourgmentTv;
+    private TextView setTv;
+    private ArrayList<String> wordsOfEncourgment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ArrayList<String> wordsOfEncourgment = new ArrayList<String>();
+
+        submitButton = findViewById(R.id.submitButton);
+        encourgmentTv = findViewById(R.id.encourgmentTv);
+        setTv = findViewById(R.id.setTv);
+
+        wordsOfEncourgment.add("hello");
+
+        //submitButton.setOnClickListener((View.OnClickListener) this);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setTv.setText(wordsOfEncourgment.get(0));
+            }
+        });
+
     }
 }
